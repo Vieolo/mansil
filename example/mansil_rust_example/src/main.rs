4e69f03263fa1d4e728f5aeff49bd4be0018866d
Ramtin <Ramtin.abadi@gmail.com>
2026-02-07 22:40:21 +0100

    Updated the rust example

--- example/mansil_rust_example/src/main.rs
+++ example/mansil_rust_example/src/main.rs
@@ -1,9 +1,9 @@
 use std::thread;
 use std::time::Duration;
-use mansil::{CURSOR_UP_1, CLEAR_LINE};
+use mansil;
 
 fn main() {
     println!("One");
     thread::sleep(Duration::from_secs(2));
-    println!("{}{}{}", CURSOR_UP_1, CLEAR_LINE, "two");
+    println!("{}{}{}", mansil::CURSOR_UP_1, mansil::CLEAR_LINE, "two");
 }
