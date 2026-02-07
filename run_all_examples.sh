a72a9e900288a78a16e480345ab90dddd3ac45bc
Ramtin <Ramtin.abadi@gmail.com>
2026-02-07 17:16:30 +0100

    Fixed the import on python example

--- run_all_examples.sh
+++ run_all_examples.sh
@@ -8,7 +8,7 @@ echo "~~~~~~~~~~~"
 
 echo "-=-=-=-= Go"
 cd mansil_go_example
-go run
+go run .
 cd ..
 echo "~~~~~~~~~~~"
 
@@ -20,7 +20,7 @@ echo "~~~~~~~~~~~"
 
 echo "-=-=-=-= Python"
 cd mansil_python_example
-uv run python -m mansil_python_example
+uv run python example_main.py
 cd ..
 echo "~~~~~~~~~~~"
 
